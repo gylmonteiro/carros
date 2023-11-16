c28a1c89b71cd2d945b5ffba6522304aeba0c9ed
Gyl Monteiro <gyl.tecno.especialista@gmail.com>
2023-11-15 22:23:36 -0300

    created first signals with send mensages to whatsapp!

--- cars/apps.py
+++ cars/apps.py
@@ -5,4 +5,5 @@ class CarsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'cars'
 
-
+    def ready(self):
+        import cars.signals
---
+++ cars/signals.py
@@ -0,0 +1,27 @@
+from django.db.models.signals import pre_save, pre_delete,post_save,post_delete
+from django.dispatch import receiver
+from cars.models import Car
+# Importações para disparar ao cadastrar um objeto no model Car
+import pyautogui as pg
+import webbrowser as web
+import time
+import pandas as pd
+@receiver(pre_save, sender=Car)
+def car_pre_save(sender, instance, **kwargs):
+    print("entrei no pré-save")
+
+@receiver(post_save, sender=Car)
+def car_pre_save(sender, instance, **kwargs):
+    phone = '55_84998181036'
+    message = 'Cadastrei um carro'
+    time.sleep(4)
+    web.open(f'https://web.whatsapp.com/send?phone={phone}&text={message}')
+    time.sleep(6)
+    width, height = pg.size()
+    pg.click(width/2, height/2)
+    time.sleep(8)
+    pg.press('esc')
+    time.sleep(5)
+    pg.press('enter')
+    time.sleep(10)
+    pg.hotkey('ctrl', 'w')
