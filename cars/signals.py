f634cbebeba22da81766c902afdee3a0c93012f4
Gyl Monteiro <gyl.tecno.especialista@gmail.com>
2023-11-22 11:34:16 -0300

    feat: tests completeds

--- cars/signals.py
+++ cars/signals.py
@@ -1,14 +1,24 @@
-from django.db.models.signals import pre_save, pre_delete,post_save,post_delete
+from django.db.models.signals import pre_save, pre_delete, post_save, post_delete
 from django.dispatch import receiver
 from cars.models import Car
+
+# import pywhatkit as wa (não vai ser necessário no momento. Somente testes)
+
+'''
 # Importações para disparar ao cadastrar um objeto no model Car
-import pyautogui as pg
-import webbrowser as web
-import time
-import pandas as pd
+# import pyautogui as pg
+# import webbrowser as web
+# import time
+# import pandas as pd
+'''
+
 @receiver(pre_save, sender=Car)
 def car_pre_save(sender, instance, **kwargs):
     print("entrei no pré-save")
+    print(instance)
+
+'''
+# método com signails para enviar mensagem via whatsapp
 
 @receiver(post_save, sender=Car)
 def car_pre_save(sender, instance, **kwargs):
@@ -20,9 +30,23 @@ def car_pre_save(sender, instance, **kwargs):
     time.sleep(6)
     width, height = pg.size()
     pg.click(width/2, height/2)
+
     time.sleep(8)
     pg.press('esc')
     time.sleep(5)
     pg.press('enter')
     time.sleep(10)
     pg.hotkey('ctrl', 'w')
+'''
+
+'''
+# Uma maneira de enviar com a biblioteca pywhatkit
+@receiver(post_save, sender=Car)
+def car_post_save(sender, instance, **kwargs):
+    phone = '+5584998181036'
+    name_car = instance.model
+    message = f'Esse é um teste com o cadatro do carro {name_car}'
+    wa.sendwhatmsg_instantly(phone, message, tab_close=True, wait_time=20, close_time=5)
+    # return instance
+'''
+
