7b49427256654975b9f4297be00a435cb22d1426
Gyl Monteiro <gyl.tecno.especialista@gmail.com>
2023-11-24 16:15:59 -0300

    feat: modifided Allowed_ hosts

--- app/settings.py
+++ app/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-g&m!o&+#cl40%=su7(=ow7%=rc*)x$zcx2_l!%fi6@(!k(2l_!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
